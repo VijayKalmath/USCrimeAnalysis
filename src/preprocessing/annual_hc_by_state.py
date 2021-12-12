1b260c36d72b53f64e15a79023647f0bb7399bb8
smaranjitghose <46641503+smaranjitghose@users.noreply.github.com>
2021-12-12 13:53:59 -0500

    Added Script for HC by Offense Type

--- src/preprocessing/annual_hc_by_state.py
+++ src/preprocessing/annual_hc_by_state.py
@@ -14,7 +14,7 @@ def main():
     # Create a dataframe to store the result and initialize it with the values for the first year
     df_res = get_state_crime_count(file_paths[0])
     # Iterate over the rest of the files
-    for p in tqdm(file_paths[2:]):
+    for p in tqdm(file_paths[1:]):
         df_temp = get_state_crime_count(p)
         df_res = pd.merge(df_res,df_temp,how="left",on=["States"])
     # Save the result to disk
@@ -31,18 +31,18 @@ def get_state_crime_count(path: str) -> pd.DataFrame:
     t_name = " ".join(path[path.index("Table"): path.index("_Offenses")].split("_"))
     t_year = path[path.index(".xls") - 4: path.index(".xls")]
     # Read the dataframe
-    df_state_count = pd.read_excel(path, sheet_name=t_name)
+    df = pd.read_excel(path, sheet_name=t_name)
     # Get the start and end indices of the interested datapoints
-    start = df_state_count.index[df_state_count[t_name] == "Total"][0] + 1
-    end = df_state_count.index[df_state_count[t_name] == "Wyoming"][0] + 1
+    start = df.index[df[t_name] == "Total"][0] + 1
+    end = df.index[df[t_name] == "Wyoming"][0] + 1
     # Slice the dataset
-    df_state_count = df_state_count.iloc[start: end, 0:2]
+    df = df.iloc[start: end, 0:2]
     # Reset the index
-    df_state_count.reset_index(drop=True, inplace=True)
+    df.reset_index(drop=True, inplace=True)
     # Rename the columns
-    df_state_count.rename(columns={t_name: "States", "Unnamed: 1": t_year}, inplace=True)
+    df.rename(columns={t_name: "States", "Unnamed: 1": t_year}, inplace=True)
     # Return the result
-    return df_state_count
+    return df
 
 if __name__ == "__main__":
     main()
